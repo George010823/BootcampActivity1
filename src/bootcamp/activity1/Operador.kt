eb4a1945429bbe42e00056a0fccdbaf9e0062ea3
Jorge Ivan Gomez <jorgomezre@unal.edu.co>
2022-05-01 07:13:20 -0500

    Se inserta la logica de la clase hija Operador y se crea la clsase principal main

--- src/bootcamp/activity1/Operador.kt
+++ src/bootcamp/activity1/Operador.kt
@@ -1,4 +1,15 @@
 package bootcamp.activity1
 
-class Operador {
+class Operador: Empleado(), AccionEmpleado {
+    // Operador se gana 10 por hora y trabaja 230 horas al mes
+
+    override val valorHora = 10
+    override val cantHoraMes = 230
+    override var cantHoraAdicional = 10
+    override val salarioBase = valorHora * cantHoraMes
+    override var bonus = valorHora * cantHoraAdicional
+
+    override fun calcularSalario() {
+        println("Salario total del gerente es: ${salarioBase + bonus}")
+    }
 }
--- src/bootcamp/activity1/main.kt
+++ src/bootcamp/activity1/main.kt
@@ -1,2 +1,14 @@
 package bootcamp.activity1
 
+fun main(args: Array<String>){
+    calcularSalarioEmpleado()
+}
+
+fun calcularSalarioEmpleado(){
+    val gerente = Gerente()
+    val contador = Contador()
+    val operador = Operador()
+
+    println("Salario total del gerente: ${gerente.cantHoraMes}")
+    gerente.calcularSalario()
+}
